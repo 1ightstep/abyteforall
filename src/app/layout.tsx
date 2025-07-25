e5efd8e8a30970ae5622227126732c3d4b6559ac
lightsteps <reallightsteps@gmail.com>
2025-07-25 12:59:57 -0700

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { geistSans } from "@/app/ui/font";
 import { LenisProvider } from "@/components/LenisProvider";
 import "@/lib/fontawesome";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "A Byte For All",
